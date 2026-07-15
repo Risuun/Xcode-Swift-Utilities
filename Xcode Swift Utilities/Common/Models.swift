f6ada689a04589c97a83032624df4bc51ab7696a
Michael Kubilis <risuun@gmail.com>
2026-07-15 15:57:19 -0500

    1.1.0

--- Xcode Swift Utilities/Common/Models.swift
+++ Xcode Swift Utilities/Common/Models.swift
@@ -5,7 +5,6 @@ import Foundation
 public struct SourceLocationModel: Codable {
     public var file: String
     public var line: Int
-    public var testChange: String = ""
     
     public init(file: String, line: Int) {
         self.file = file
